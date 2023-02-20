c5c9df5913cb0f21798f41247186568a6a60ca98
laharke <ronconimauro@yahoo.com.ar>
2023-02-19 21:08:34 -0300

    comments agregados y repartimos cartas

--- cartas.js
+++ cartas.js
@@ -1,5 +1,9 @@
 import { diccionarioValores, palos } from "./valoresCartas.js";
 
+/*
+    Funcion que crea todas las cartas de un palo.
+    Loopea y asigna un diccionario con todos los valores poniendole el numero+nombre de la carta y el valor que corresponde.
+*/
 function crearCartasPalo(palo){
     const cartasMismoPalo = [];
     for(let i = 1; i < 13; i++){
@@ -23,7 +27,9 @@ function crearCartasPalo(palo){
     }
     return cartasMismoPalo;
 }
-
+/*
+    Funcion que crea el mazo, llama la funcion crearCartasPalo 4 veces una vez por cada palo
+*/
 export const  crearMazo = () => {
     let mazo = [];
     palos.forEach(element => {
@@ -31,7 +37,9 @@ export const  crearMazo = () => {
     });
     return mazo;
 }
-
+/*
+    Mezcla el deck agarrando carta por carta y poniendola en otro lugar.
+*/
 export function mezclar(deck) {
     for (let i = 0; i < deck.length; i++) {
         let j = Math.floor(Math.random() * deck.length); // 
@@ -41,3 +49,14 @@ export function mezclar(deck) {
     }
     return deck;
 };
+
+/*
+    Reparte cartas, asigna 6 cartas de arriba para repartirselas a los jugadores.
+*/
+export function repartirCartas(deck) {
+    let cartas = [];
+    for (let i = 0; i < 6; i++){
+        cartas[i] = deck.pop();
+}
+    return cartas;
+}
--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-import { crearMazo, mezclar } from "./cartas.js";
+import { crearMazo, mezclar, repartirCartas } from "./cartas.js";
 //1 Son disti
 //cartasmismo [1copa:] 
-const mazo = crearMazo();
+let player1cards = [];
+let player2cards = [];
 
 
-console.log(mezclar(mazo));
+
+/*
+    Aca llamamos a crear mazo
+    repartimos las cartas
+*/
+let mazo = crearMazo();
+mazo = mezclar(mazo);
+let cartasARepartir = repartirCartas(mazo);
+
+/*
+    Asignamos las cartas a los jugadores
+*/
+function asignarCartas(player1cards, player2cards) {
+    for (let i = 0; i < 6; i+= 2){
+        player1cards.push(cartasARepartir[i]);
+    }
+
+    for (let i = 1; i < 6; i+= 2){
+        player2cards.push(cartasARepartir[i]);
+    }
+}
+
+asignarCartas(player1cards, player2cards);
+console.log(cartasARepartir);
+console.log(player1cards, player2cards);
+
 /*  
 
 */
